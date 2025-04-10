7361206fbb5e91ce3ff8341c02eb1f009cf30eee
김태영 <kimtaeyeong@0Taes-MacBookPro.local>
2025-04-10 21:41:17 +0900

    create cards

---
+++ app/features/community/components/post-card.tsx
@@ -0,0 +1,55 @@
+import { Link } from "react-router";
+import {
+  Card,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "~/common/components/ui/card";
+import { Avatar, AvatarImage } from "~/common/components/ui/avatar";
+import { AvatarFallback } from "@radix-ui/react-avatar";
+import { Button } from "~/common/components/ui/button";
+import { DotIcon } from "lucide-react";
+
+interface PostCardProps {
+  id: string;
+  title: string;
+  author: string;
+  authorAvatarUrl: string;
+  category: string;
+  postedAt: string;
+}
+
+export function PostCard({
+  id,
+  title,
+  author,
+  authorAvatarUrl,
+  category,
+  postedAt,
+}: PostCardProps) {
+  return (
+    <Link to={`/community/${id}`}>
+      <Card className="bg-transparent hover:bg-card/50 transition-colors">
+        <CardHeader className="flex flex-row items-center gap-2">
+          <Avatar className="size-14">
+            <AvatarFallback>{author[0]}</AvatarFallback>
+            {authorAvatarUrl && <AvatarImage src={authorAvatarUrl} />}
+          </Avatar>
+          <div className="space-y-2">
+            <CardTitle>{title}</CardTitle>
+            <div className="flex gap-2 text-sm leading-tight text-muted-foreground">
+              <span>
+                {author} on {category}
+              </span>
+              <DotIcon className="w-4 h-4" />
+              <span>{postedAt}</span>
+            </div>
+          </div>
+        </CardHeader>
+        <CardFooter className="flex justify-end">
+          <Button variant="link">Reply &rarr;</Button>
+        </CardFooter>
+      </Card>
+    </Link>
+  );
+}
--- app/root.tsx
+++ app/root.tsx
@@ -26,7 +26,7 @@ export const links: Route.LinksFunction = () => [
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en" className="dark">
+    <html lang="en">
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
@@ -44,14 +44,14 @@ export function Layout({ children }: { children: React.ReactNode }) {
 
 export default function App() {
   return (
-    <>
+    <div className="py-28">
       <Navigation
         isLoggedIn={true}
         hasNotifications={true}
         hasMessages={true}
       />
       <Outlet />
-    </>
+    </div>
   );
 }
 
